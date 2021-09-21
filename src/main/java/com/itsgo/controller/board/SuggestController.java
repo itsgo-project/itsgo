405bb5b9f58ef4adff15a6dd2df13d2723db1c20
Rolling Stone <aRollingStone27@gmail.com>
2021-09-22 00:38:09 +0900

    Suggest 게시판 리스트

--- src/main/java/com/itsgo/controller/board/SuggestController.java
+++ src/main/java/com/itsgo/controller/board/SuggestController.java
@@ -1,9 +1,14 @@
 package com.itsgo.controller.board;
 
+import com.itsgo.domain.board.BoardSuggest;
 import com.itsgo.mapstuct.BoardSuggestMapper;
 import com.itsgo.service.board.BoardSuggestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,14 +18,15 @@ public class SuggestController
 {
     private final BoardSuggestService boardService;
     private final BoardSuggestMapper boardMapper;
-//
-//    @RequestMapping("/getQBoardList")
-//    public String getQBoardList(@PageableDefault Pageable pageable, Model model)
-//    {
-//        Page<QBoard> qBoardList = qBoardService.getQBoardList(pageable);
-//        model.addAttribute("qBoardList", qBoardList);
-//        return "getQBoardList";
-//    }
+
+    @RequestMapping("/getBoardList")
+    public String getQBoardList(@PageableDefault Pageable pageable, Model model)
+    {
+        Page<BoardSuggest> boardList = boardService.getBoardList(pageable);
+        model.addAttribute("boardList", boardList);
+        return "/board/suggest/getBoardList";
+    }
+
 //
 //    @GetMapping("/insertQBoard")
 //    public String insertQBoardView()
